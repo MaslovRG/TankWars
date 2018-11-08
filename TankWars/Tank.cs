561afe976d0f20e518853f9e537082984ce04c7a
MaslovRG <al.maslovrg@gmail.com>
2018-11-08 16:08:41 +0300

    Add tank class

---
+++ TankWars/Tank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankWars
+{
+    public class Tank
+    {
+        public int Armor;
+        public int Health;
+        public int Damage; 
+
+        public Tank(int nArmor, int nHealth, int nDamage)
+        {
+            Armor = nArmor;
+            Health = nHealth;
+            Armor = nArmor; 
+        }
+
+        public void Shoot(Tank EnemyTank)
+        {
+            EnemyTank.Health += Armor - Damage; 
+        }
+
+        public void Repair()
+        {
+            Health += 5; 
+        }
+    }
+}
